dcc4b4696500244ea191c78a65c9e0ec5fe224ff
Kelvin Lau <kelvin.kl.lau@gmail.com>
2026-09-05 19:01:53 -0700

    Target macOS 27 with Swift tools 6.4

--- GmailAPI/Package.swift
+++ GmailAPI/Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
   name: "GmailAPI",
-  platforms: [.macOS(.v14)],
+  platforms: [.macOS(.v27)],
   products: [
     .library(name: "GmailAPI", targets: ["GmailAPI"])
   ],
